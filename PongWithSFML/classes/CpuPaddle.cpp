f95f237bcef72b180368a9452ae1e3be1eec9c13
TheEliteD <dhristov06@gmail.com>
2024-11-03 03:00:52 +0200

    tried to fux the stuttering of the bot

--- PongWithSFML/classes/CpuPaddle.cpp
+++ PongWithSFML/classes/CpuPaddle.cpp
@@ -7,10 +7,10 @@ CpuPaddle::CpuPaddle(float startX, float startY) : Paddle(startX, startY) {
 void CpuPaddle::Update(sf::Time dt, Ball& ball) {
 	mPosition.y = std::clamp(mPosition.y, mShape.getSize().y / 2, 720 - mShape.getSize().y / 2);
 
-	if (mShape.getPosition().y + (float)mHeight / 2 > ball.getPosition().top) {
+	if (mShape.getPosition().y + (float)mHeight / 2 > ball.getPosition().top + ball.getPosition().height){
 		mPosition.y -= mSpeed * dt.asSeconds();
 	}
-	else if (mShape.getPosition().y + (float)mHeight / 2 <= ball.getPosition().top) {
+	else if (mShape.getPosition().y + (float)mHeight / 2 <= ball.getPosition().top + ball.getPosition().height) {
 		mPosition.y += mSpeed * dt.asSeconds();
 	}
 
